84f3b61f12f03c0a6884a83c7863f38a329456c4
Thanh Quang Vo <82924654+BigCat3997@users.noreply.github.com>
2025-01-23 19:44:57 +0700

    Update modify_env.py

--- pipelines/modify_env.py
+++ pipelines/modify_env.py
@@ -4,6 +4,7 @@ import os
 os.environ['GLOBAL_VAR1'] = 'modified_value1'
 os.environ['GLOBAL_VAR2'] = 'modified_value2'
 
-# Print the modified environment variables
-print(f"GLOBAL_VAR1={os.environ['GLOBAL_VAR1']}")
-print(f"GLOBAL_VAR2={os.environ['GLOBAL_VAR2']}")
+# Write the modified environment variables to a file
+with open('env_vars.txt', 'w') as f:
+    f.write(f"GLOBAL_VAR1={os.environ['GLOBAL_VAR1']}\n")
+    f.write(f"GLOBAL_VAR2={os.environ['GLOBAL_VAR2']}\n")
